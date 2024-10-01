0e0874c9bf22df9716c1681fab1c7112080c45bd
VanLeDinh96 <dinhvanle.it@gmail.com>
2024-10-01 08:09:31 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { RouterOutlet } from '@angular/router';
 import {ButtonModule} from "primeng/button";
 import {FaIconLibrary, FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 import {fontAwesomeIcons} from "./shared/font-awesome-icons";
+import {NavbarComponent} from "./layout/navbar/navbar.component";
+import {FooterComponent} from "./layout/footer/footer.component";
 import {ToastModule} from "primeng/toast";
 import {ToastService} from "./layout/toast.service";
 import {MessageService} from "primeng/api";
@@ -10,7 +12,7 @@ import {MessageService} from "primeng/api";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule, FontAwesomeModule, ToastModule],
+  imports: [RouterOutlet, ButtonModule, FontAwesomeModule, NavbarComponent, FooterComponent, ToastModule],
   providers: [MessageService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,18 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {provideHttpClient, withInterceptors, withXsrfConfiguration} from "@angular/common/http";
 import { provideAnimations } from '@angular/platform-browser/animations';
+import {authExpired} from "./core/auth/auth-expired.interceptor";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimations(), 
+    provideHttpClient(
+      withInterceptors([authExpired]),
+      withXsrfConfiguration(
+        {cookieName: "XSRF-TOKEN", headerName: "X-XSRF-TOKEN"}),
+    ),
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes)
   ]
